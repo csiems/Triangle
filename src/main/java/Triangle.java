357ff30497968d095f1b9d29680f6fd84ca42708
Chris Siems and Illia Kuchko <ikuchko@gmail.com>
2016-01-11 10:25:03 -0800

    pass all unit tests

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,12 +1,75 @@
-import java.util.Map;
-import java.util.HashMap;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
-import java.util.Random;
+
 
 public class Triangle {
+  private int mSideOne;
+  private int mSideTwo;
+  private int mSideThree;
 
+  public Triangle (int sideOne, int sideTwo, int sideThree){
+    mSideOne = sideOne;
+    mSideTwo = sideTwo;
+    mSideThree = sideThree;
+    }
 
-  public Triangle (int sideOne, int sideTwo, int sideThree){}
+    public int getSideOne(){
+      return mSideOne;
+    }
+
+    public int getSideTwo(){
+      return mSideTwo;
+    }
+
+    public int getSideThree(){
+      return mSideThree;
+    }
+
+    public boolean isATriangle() {
+        if ((mSideTwo + mSideThree > mSideOne) && (mSideOne + mSideThree > mSideTwo) && (mSideOne + mSideTwo > mSideThree)) {
+          return true;
+        } else {
+          return false;
+        }
+    }
+
+    public boolean isEquilateralTriangle() {
+      if (mSideOne == mSideTwo && mSideOne == mSideThree && mSideTwo == mSideThree) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public boolean isIsoscelesTriangle(){
+      if (!isEquilateralTriangle()){
+        if (mSideOne == mSideTwo || mSideOne == mSideThree || mSideTwo == mSideThree){
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    public boolean isScaleneTriangle(){
+      if (!isEquilateralTriangle() && !isIsoscelesTriangle()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public String whatTriangleType() {
+      if (isATriangle()) {
+        if (isEquilateralTriangle()) {
+          return "equilateral";
+        } else if (isIsoscelesTriangle()) {
+          return "isosceles";
+        } else {
+          return "scalene";
+        }
+      } else {
+      return "This is not a triangle!";
+      }
+    }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -9,5 +9,57 @@ public class TriangleTest {
     assertEquals(true, testTriangle instanceof Triangle);
   }
 
+  @Test
+  public void newTriangle_setsSideOne() {
+    Triangle testTriangle = new Triangle(3, 4, 5);
+    assertEquals(3, testTriangle.getSideOne());
+  }
 
+  @Test
+  public void newTriangle_setsSideTwo() {
+    Triangle testTriangle = new Triangle(3, 4, 5);
+    assertEquals(4, testTriangle.getSideTwo());
+  }
+
+  @Test
+  public void newTriangle_setsSideThree() {
+    Triangle testTriangle = new Triangle(3, 4, 5);
+    assertEquals(5, testTriangle.getSideThree());
+  }
+
+  @Test
+  public void newTriangle_isATriangle_true(){
+    Triangle testTriangle = new Triangle(2, 2, 3);
+    assertEquals(true, testTriangle.isATriangle());
+  }
+
+  @Test
+  public void newTriangle_isATriangle_false(){
+    Triangle testTriangle = new Triangle(2, 2, 8);
+    assertEquals(false, testTriangle.isATriangle());
+  }
+
+  @Test
+  public void newTriangle_isEquilateralTriangle_true(){
+    Triangle testTriangle = new Triangle(2, 2, 2);
+    assertEquals(true, testTriangle.isEquilateralTriangle());
+  }
+
+  @Test
+  public void newTriangle_isIsoscelesTriangle_true(){
+    Triangle testTriangle = new Triangle(2, 2, 3);
+    assertEquals(true, testTriangle.isIsoscelesTriangle());
+  }
+
+  @Test
+  public void newTriangle_isScaleneTriangle_true(){
+    Triangle testTriangle = new Triangle(2, 3, 4);
+    assertEquals(true, testTriangle.isScaleneTriangle());
+  }
+
+  @Test
+  public void newTriangle_whatTriangleType_ScaleneTriangle(){
+    Triangle testTriangle = new Triangle(2, 3, 4);
+    assertEquals("scalene", testTriangle.whatTriangleType());
+  }
 }
